97b9c4c53fdb4dec26be1bd8b8790489815fba6d
Salari, AmirHossein <amir227044@gmail.com>
2023-09-30 19:49:24 +0330

    Initial commit

---
+++ Session-1/javascript-learn-1.js
@@ -0,0 +1,18 @@
+var a = 1 //This is a number
+var b = 2.5 //This is a float 
+var c = "Salam" //This is a string
+var d = a + 2 //This is sum
+
+var e = false
+
+if (e) {
+    console.log('Man dakhel shart hastam')
+} else {
+    console.log('Man dakhel shart nistam!!!!!!!!!')
+}
+
+
+
+
+
+

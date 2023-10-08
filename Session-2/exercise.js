09cd8dea1e47335738aa72c09b69bcc8ba9184fd
eshr4q <feshr4q@gmail.com>
2023-10-08 20:32:37 +0330

    with only 1 input

---
+++ Session-2/exercise.js
@@ -0,0 +1,20 @@
+var names=["mamad", "samad", "ahad", "vahad"];
+
+function nameChecker(input){
+    var output=("bood");
+
+   if (names.includes(input)){
+        
+        output = ("bood");
+   }
+   else {
+    output = ("nabood ke");
+   }
+
+
+    
+   console.log(output)
+}
+
+
+nameChecker("manad");
